e836a57141a58334a39f5dd2bb56dbacab02cb89
tobias <tobias.madlberger@gmail.com>
2023-11-25 23:22:58 +0100

    Added accelerometer and digitals

--- main.cpp
+++ main.cpp
@@ -16,6 +16,24 @@ void add_value_route(httplib::Server &svr, const char *route, signed short (*val
     });
 }
 
+void add_digital_value_route(httplib::Server &svr, const char *route, int (*digital_value_function)(int)) {
+    svr.Get(route, [digital_value_function](const httplib::Request &req, httplib::Response &res) {
+        try {
+            auto json_body = json::parse(req.body);
+            if (json_body.find("port") == json_body.end())
+                throw std::runtime_error("Missing port");
+
+            int port = json_body["port"];
+            json data;
+            data["value"] = digital_value_function(port);
+            res.set_content(data.dump(), "application/json");
+        } catch (...) {
+            res.status = 400;
+            res.set_content("Bad Request: Invalid JSON payload", "text/plain");
+        }
+    });
+}
+
 int main() {
     using namespace httplib;
     void *lib_handle = dlopen("/usr/lib/libkipr.so", RTLD_LAZY);
@@ -34,6 +52,11 @@ int main() {
     add_value_route(svr, "/magneto_y", (signed short (*)()) dlsym(lib_handle, "magneto_y"));
     add_value_route(svr, "/magneto_z", (signed short (*)()) dlsym(lib_handle, "magneto_z"));
 
+    add_value_route(svr, "/accel_x", (signed short (*)()) dlsym(lib_handle, "accel_x"));
+    add_value_route(svr, "/accel_y", (signed short (*)()) dlsym(lib_handle, "accel_y"));
+    add_value_route(svr, "/accel_z", (signed short (*)()) dlsym(lib_handle, "accel_z"));
+
+    add_digital_value_route(svr, "/get_digital_value", (int (*)(int)) dlsym(lib_handle, "get_digital_value"));
 
     svr.listen("0.0.0.0", PORT);
     dlclose(lib_handle);
